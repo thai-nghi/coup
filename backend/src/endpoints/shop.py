9f281920c3eb86d8d9d0860f87e2207493ebb95e
thai-nghi <pthai.nghi@gmail.com>
2024-11-20 11:05:49 -0800

    update tags

--- backend/src/endpoints/shop.py
+++ backend/src/endpoints/shop.py
@@ -2,14 +2,15 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, Path
 from sqlalchemy.ext.asyncio import AsyncSession
-
 from src import schemas
 from src.dependencies.database import get_db
 from src.dependencies.user import get_current_user
 from src.exceptions import BadRequestException
 from src.services import shop as shop_service
 
-router = APIRouter(prefix="/shop", dependencies=[Depends(get_current_user)])
+router = APIRouter(
+    prefix="/shop", dependencies=[Depends(get_current_user)], tags=["shop"]
+)
 
 
 @router.get("/")
