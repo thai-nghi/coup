27725703234f62dfd30360e8b947d9b119f37c54
thai-nghi <pthai.nghi@gmail.com>
2024-12-07 13:54:21 -0800

    edit config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,12 @@ module.exports = {
       // Warning: This allows production builds to successfully complete even if
       // your project has ESLint errors.
       ignoreDuringBuilds: true,
-      ignoreBuildErrors: true,
     },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+      },
   }
